6429e63fb6b0aecc63b9f13a15cee69fefdf45d2
yanjingzhaisun <yanjingzhaisun@gmail.com>
2015-09-27 17:37:56 -0400

    initial project version

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//Creates a new Phaser Game
+//You might want to check here to understand the basics of Phaser: http://www.photonstorm.com/phaser/tutorial-making-your-first-phaser-game
+                        
+var game = new Phaser.Game(500, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render});
+
+
+  function preload () {    
+  }
+
+  function create () {
+      // game.physics.startSystem(Phaser.Physics.ARCADE);  
+  }
+
+  function update(){
+  }
+
+  function render(){
+
+  }
