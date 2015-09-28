0fb56bc4c6e0b8430c83673caa4cf3b1c2ada3d7
yanjingzhaisun <yanjingzhaisun@gmail.com>
2015-09-28 16:55:08 -0400

    Hitballs has been added, several test being done. TODO: random generate speed adjustment graphics

--- script.js
+++ script.js
@@ -4,26 +4,52 @@
                         
 var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render});
 
+  //constant section
+  var fCircleRadium = 100;
+  var fHitBallRadium = 25;
+
+  //string and status section
   var sGameStatus;
   var sGamePlayingStatus;
+
+
+  //sprite section
   var spriteBackground;
   var spritePlayerBall;
+  var groupHitBalls;
 
+  //collision group
+  var groupHitBallsCollision;
+  var groupPlayerCollision;
+
+  //bool section
   var isMouseInCircle = false;
 
-  var fCircleRadium = 100;
-
+  //float variables section
   var fMouseAngle = 0;
+
+  //debug variables section
+  var debugResult = 'click a body'; 
+
 
   function preload () {
     game.load.image('bg', 'assets/image/background.png');   
     game.load.image('player', 'assets/image/ball.png');
+    game.load.image('hitBallw','assets/image/white_ball.png');
+    game.load.image('hitBallb','assets/image/black_ball.png');
   }
 
   function create () {
     // game.physics.startSystem(Phaser.Physics.ARCADE); 
+    game.physics.startSystem(Phaser.Physics.P2JS);
     game.world.setBounds(0, 0, 800, 800);
+    game.physics.p2.setImpactEvents(true);
 
+    //collision group
+    groupHitBallsCollision = game.physics.p2.createCollisionGroup();
+    groupPlayerCollision = game.physics.p2.createCollisionGroup();
+
+    //create sprites.
     spriteBackground = game.add.sprite(game.width/2, game.height/2, 'bg');
     spriteBackground.anchor.setTo(0.5, 0.5);
     spriteBackground.visible = false;
@@ -32,6 +58,23 @@ var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create
     spritePlayerBall.anchor.setTo(0.5, 0.5);
     spritePlayerBall.visible = false;
 
+    //set groups
+    groupHitBalls = game.add.group();
+    groupHitBalls.enableBody = true;
+    groupHitBalls.physicsBodyType = Phaser.Physics.P2JS;
+
+
+    //set player body and collides
+    game.physics.p2.enable(spritePlayerBall);
+    spritePlayerBall.body.setCircle(100);
+    spritePlayerBall.body.fixedRotation = true;
+    spritePlayerBall.body.setCollisionGroup(groupPlayerCollision);
+    spritePlayerBall.body.collides(groupHitBallsCollision, actionGetCollided, this);
+
+    //debug
+    game.input.onDown.add(debugClick, this);
+
+    //initialization
     sGameStatus = 'GAME_MENU'; 
     initialState();
   }
@@ -52,7 +95,7 @@ var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create
   }
 
   function render(){
-
+    game.debug.text(debugResult, 32, 32);
   }
 
   function initialState(){
@@ -61,6 +104,9 @@ var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create
     } else if (sGameStatus == 'GAME_PLAYING') {
       spriteBackground.visible = true;
       spritePlayerBall.visible = true;
+      //timerEventMakeBall.lastTime = 0;
+
+      game.time.events.repeat(Phaser.Timer.SECOND * 4, 10, timerEventMakeBall, this);
     } else if (sGameStatus == 'GAME_OVER') {
 
     } else {
@@ -82,6 +128,7 @@ var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create
       spriteBackground.angle = fMouseAngle;
       spritePlayerBall.angle = fMouseAngle;
     }
+    //timerEventMakeBall();
   }
 
   function updateGameOver(){
@@ -92,13 +139,21 @@ var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create
 
   }
 
+  // todo:
+  // rythm things
+  function timerEventMakeBall(){
+    if (sGameStatus != 'GAME_PLAYING') return;
+    console.log("making timer balls now");
+    makeHitBall(0, 0);
+  }
+
   function getIsMouseInCircle(){
 
     var distance = Math.sqrt(
         ((game.input.mousePointer.x - game.width/2) * (game.input.mousePointer.x - game.width/2)) +
          ((game.input.mousePointer.y - game.height/2) * (game.input.mousePointer.y - game.height/2))
          )
-    console.log("Mouse distance is " + distance);
+    //console.log("Mouse distance is " + distance);
     if (distance < fCircleRadium) {
       isMouseInCircle = true;
       return true;
@@ -114,3 +169,48 @@ var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create
 
     return fMouseAngle;
   }
+
+  // todo: 
+  // emerge positions
+  // hitball color
+  // set speed 
+  function makeHitBall(posx, posy){
+    var hitBall = groupHitBalls.create(posx, posy, 'hitBallb');
+    hitBall.anchor.setTo(0.5, 0.5);
+    hitBall.body.setCircle(fHitBallRadium);
+    hitBall.body.setCollisionGroup(groupHitBallsCollision);
+    hitBall.body.collides(groupPlayerCollision);
+    
+    // todo:
+    // curious thing is hitBall.body.speed generally lower and lower
+    hitBall.body.velocity.x = 40;
+    hitBall.body.velocity.y = 40;
+  }
+
+  // todo:
+  // rightnow just print that it is collided.
+  function actionGetCollided(body1, body2){
+    //body1 is player, body2 is hitball
+    console.log("get hit");
+
+    //body2.sprite is indeed in the groupHitBalls
+    groupHitBalls.remove(body2.sprite, true);
+    //console.log("and after the remove the new length is " + groupHitBalls.length);
+
+  }
+
+
+  // debug functions section
+  function debugClick(pointer){
+    var bodies = game.physics.p2.hitTest(pointer.position, [spritePlayerBall]);
+    if (bodies.length == 0) {
+      debugResult = "You didn't click a body.";
+    } else {
+      debugResult = "You clicked: ";
+      for (var i = 0; i < bodies.length; i++) {
+        debugResult += bodies[i].parent.sprite.key;
+        if (i < bodies.length - 1) debugResult += ',';
+      } 
+    }
+  }
+
