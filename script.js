f0060a83b48cadda0065874d7bd6e6327401a379
yanjingzhaisun <yanjingzhaisun@gmail.com>
2015-09-28 11:32:40 -0400

    This version add background, while this background is shown by sprite. Player ned to changed the angle of background (not implemented yet)

--- script.js
+++ script.js
@@ -2,25 +2,73 @@
 //Creates a new Phaser Game
 //You might want to check here to understand the basics of Phaser: http://www.photonstorm.com/phaser/tutorial-making-your-first-phaser-game
                         
-var game = new Phaser.Game(500, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render});
+var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render});
 
   var sGameStatus;
+  var sGamePlayingStatus;
+  var spriteBackground;
 
-  function preload () {    
+  function preload () {
+    game.load.image('bg', 'assets/iamge/background.png');    
   }
 
   function create () {
     // game.physics.startSystem(Phaser.Physics.ARCADE); 
+    game.world.setBounds(0, 0, 800, 800);
+
+    spriteBackground = game.add.sprite(game.width/2, game.height/2, 'bg');
+    spriteBackground.anchor.setTo(0.5, 0.5);
+
+    spriteBackground.visible = false;
+
     sGameStatus = 'GAME_MENU'; 
+    initialState();
   }
 
   function update(){
     if (sGameStatus == 'GAME_MENU') {
-
+      //Start menual
+      updateGameMenu();  
+    } else if (sGameStatus == 'GAME_PLAYING'){
+      //main game. much detailed status machine needed.
+      updateGamePlaying();
+    } else if (sGameStatus == 'GAME_OVER') {
+      //game over menu staff
+      updateGameOver();
+    } else {
+      //else thing;
     }
-    else {}
   }
 
   function render(){
+
+  }
+
+  function initialState(){
+    if (sGameStatus == 'GAME_MENU') {
+
+    } else if (sGameStatus == 'GAME_PLAYING') {
+      spriteBackground.visible = true;
+    } else if (sGameStatus == 'GAME_OVER') {
+
+    } else {
+
+    }
+  }
+
+  function updateGameMenu(){
+    sGameStatus = 'GAME_PLAYING';
+    initialState();
+  }
+
+  function updateGamePlaying(){
+
+  }
+
+  function updateGameOver(){
+
+  }
+
+  function buttonStart(){
 
   }
